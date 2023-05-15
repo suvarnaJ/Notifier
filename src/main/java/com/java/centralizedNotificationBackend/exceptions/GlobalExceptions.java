c71202912af330ead91bba096ad56d58f22d6160
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-05-15 18:00:42 +0530

    Fixed some issue

--- src/main/java/com/java/centralizedNotificationBackend/exceptions/GlobalExceptions.java
+++ src/main/java/com/java/centralizedNotificationBackend/exceptions/GlobalExceptions.java
@@ -4,11 +4,8 @@ import com.java.centralizedNotificationBackend.payload.ErrorResponse;
 import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-=======
->>>>>>> 72b8fa0a0f1bdbda5e0a7e524b62ba09b8ca264b
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,7 +16,6 @@ public class GlobalExceptions {
     public ResponseEntity<?> ExpiredJwtException(ExpiredJwtException ex){
         return ErrorResponse.errorHandler(HttpStatus.BAD_REQUEST,true,ex.getMessage());
     }
-<<<<<<< HEAD
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<?> HttpMessageNotReadableException(HttpMessageNotReadableException ex){
@@ -48,6 +44,4 @@ public class GlobalExceptions {
             return response;
         }
     }
-=======
->>>>>>> 72b8fa0a0f1bdbda5e0a7e524b62ba09b8ca264b
 }
