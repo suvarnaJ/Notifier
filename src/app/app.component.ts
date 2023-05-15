5854b2426caa67cf7161a2820280f3b5d9f64a54
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-05-15 17:54:48 +0530

    Fixed Some isuue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,6 +51,7 @@ export class AppComponent implements OnInit {
 
     idle.onIdleStart.subscribe(() => {
       this.idleState = 'You have gone Idle';
+      console.log(this.idleState);
       this.childModal.show();
     }); // this is place to do somthing when user goes Idle.
 
