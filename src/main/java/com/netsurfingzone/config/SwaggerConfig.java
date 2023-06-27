2c9b172e17b5ccceca8daf5fda0351694115cd79
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-06-27 11:12:00 +0530

    Avoid multiplehit api

---
+++ src/main/java/com/netsurfingzone/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.netsurfingzone.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.netsurfingzone.producer")).build();
+    }
+
+    private ApiInfo getInfo(){
+        return new ApiInfo("Centralized Notification Engine","","1.0","Terms of Service",null,"License of APIS","API license URL", Collections.emptyList());
+    }
+
+}
