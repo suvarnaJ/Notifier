f434f5022456524cdb6c5e4733d800f15af58cc5
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-06-28 12:20:38 +0530

    Add Event Name Validations

--- src/main/java/com/netsurfingzone/exception/GlobalExceptionHandler.java
+++ src/main/java/com/netsurfingzone/exception/GlobalExceptionHandler.java
@@ -4,29 +4,31 @@ import java.util.Date;
 
 import com.azure.core.exception.ResourceNotFoundException;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.netsurfingzone.payload.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> globalExcpetionHandler(Exception ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<?> HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex, WebRequest request) {
+        return ErrorResponse.errorHandler(HttpStatus.BAD_REQUEST, true, "Invalid method");
     }
 
     @ExceptionHandler(JsonParseException.class)
-    public ResponseEntity<?> JsonMappingException(JsonParseException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<?> JsonParseException(JsonParseException ex, WebRequest request) {
+        return ErrorResponse.errorHandler(HttpStatus.BAD_REQUEST, true, "Invalid payload");
     }
 }
