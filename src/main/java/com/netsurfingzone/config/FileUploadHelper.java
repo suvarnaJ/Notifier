7d85b09424eab09f31c248c734443df58e6a722e
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-07-14 19:22:05 +0530

    modification in summary notification template

--- src/main/java/com/netsurfingzone/config/FileUploadHelper.java
+++ src/main/java/com/netsurfingzone/config/FileUploadHelper.java
@@ -15,8 +15,8 @@ import java.nio.file.Files;
 @Component
 public class FileUploadHelper {
 
-    public final String UPLOAD_DIR="/TCLNotifier/repo/Notifier/src/main/resources/static/files";
-
+    public final String UPLOAD_DIR_PRODUCTION="/TCLNotifier/repo/Notifier/src/main/resources/static/files";
+    public final String UPLOAD_DIR_LOCAL="src\\main\\resources\\static\\files";
 
     public boolean uploadFile(MultipartFile multipartFile, HttpServletRequest request){
         boolean f = false;
@@ -26,7 +26,7 @@ public class FileUploadHelper {
             byte data[] = new byte[is.available()];
             is.read(data);
             //write
-            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename());
+            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR_LOCAL+File.separator+multipartFile.getOriginalFilename());
             fos.write(data);
             fos.flush();
             fos.close();
@@ -41,7 +41,7 @@ public class FileUploadHelper {
 
     public String load(String filename) throws IOException {
         try {
-            File file = ResourceUtils.getFile(UPLOAD_DIR+File.separator+filename);
+            File file = ResourceUtils.getFile(UPLOAD_DIR_LOCAL+File.separator+filename);
             String content = new String(Files.readAllBytes(file.toPath()));
             return content;
         } catch (MalformedURLException e) {
