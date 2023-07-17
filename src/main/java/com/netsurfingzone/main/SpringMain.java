acc38f2a3de74bbcc9bbbca17ed5e3162066c1d8
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-07-17 14:57:50 +0530

    modification in summary notification template

--- src/main/java/com/netsurfingzone/main/SpringMain.java
+++ src/main/java/com/netsurfingzone/main/SpringMain.java
@@ -1,13 +1,20 @@
 package com.netsurfingzone.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.netsurfingzone.*")
 public class SpringMain {
+
+	@Autowired
+    private static JdbcTemplate jdbcTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMain.class, args);
+		jdbcTemplate.execute("CREATE TABLE CN_LOG_ERROR(" + "AccountName VARCHAR(255), Status NUMERIC(3), Message VARCHAR(255), API_Name VARCHAR(255), Created_At VARCHAR(255))");
 	}
 }
