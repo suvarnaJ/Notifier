27a6e7f86f63ba9485b5c73ef754f3a478a603ba
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-07-18 10:52:07 +0530

    modification in summary notification template

--- src/main/java/com/netsurfingzone/config/SpringKafkaConfig.java
+++ src/main/java/com/netsurfingzone/config/SpringKafkaConfig.java
@@ -71,6 +71,8 @@ public class SpringKafkaConfig {
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "com.netsurfingzone.dto");
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "com.netsurfingzone.dto.SummaryPayload");
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+		configMap.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 300000);
+		configMap.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 50);
 		//configMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaNotifyDeserializer.class);
 		return new DefaultKafkaConsumerFactory<>(configMap);
 	}
