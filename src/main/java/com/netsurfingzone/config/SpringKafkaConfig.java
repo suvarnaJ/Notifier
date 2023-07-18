5c6699cc6a6f0edebde099563a6e0408467bdd51
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-07-18 14:38:59 +0530

    Minor change

--- src/main/java/com/netsurfingzone/config/SpringKafkaConfig.java
+++ src/main/java/com/netsurfingzone/config/SpringKafkaConfig.java
@@ -36,7 +36,7 @@ public class SpringKafkaConfig {
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES,"com.netsurfingzone.dto.SummaryPayload");
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES,"*");
 		configMap.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,1000);
-		configMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, 8000);
+		configMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
 		configMap.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,180000);
 		configMap.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 2147483647);
 		configMap.put(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, 181000);
@@ -59,7 +59,7 @@ public class SpringKafkaConfig {
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "com.netsurfingzone.dto.Notify");
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
 		configMap.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,1000);
-		configMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, 8000);
+		configMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
 		configMap.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,180000);
 		configMap.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 2147483647);
 		configMap.put(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, 181000);
@@ -78,7 +78,7 @@ public class SpringKafkaConfig {
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "com.netsurfingzone.dto.SummaryPayload");
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
 		configMap.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,1000);
-		configMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, 8000);
+		configMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
 		configMap.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,180000);
 		configMap.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 2147483647);
 		configMap.put(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, 181000);
