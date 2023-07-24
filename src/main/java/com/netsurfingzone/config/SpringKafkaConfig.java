9d9fcf366ba5713f9ce70f45e1edad1636e08487
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-07-24 15:26:30 +0530

    Minor Changes

--- src/main/java/com/netsurfingzone/config/SpringKafkaConfig.java
+++ src/main/java/com/netsurfingzone/config/SpringKafkaConfig.java
@@ -56,8 +56,8 @@ public class SpringKafkaConfig {
 		configMap.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,"700000");
 		configMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");
 		configMap.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,"5000");
-		configMap.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG,"20000");
-		configMap.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"1");
+		configMap.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG,"3600000");
+		configMap.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"10");
 		configMap.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");
 		//configMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaNotifyDeserializer.class);
 		return new DefaultKafkaConsumerFactory<>(configMap);
@@ -76,8 +76,8 @@ public class SpringKafkaConfig {
 		configMap.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,"700000");
 		configMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");
 		configMap.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,"5000");
-		configMap.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG,"20000");
-		configMap.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"1");
+		configMap.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG,"3600000");
+		configMap.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"10");
 		configMap.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");
 		//configMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaNotifyDeserializer.class);
 		return new DefaultKafkaConsumerFactory<>(configMap);
