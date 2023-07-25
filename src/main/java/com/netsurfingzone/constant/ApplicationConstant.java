8ee77dcff7f6e62843c00b3b42797077f4a0793b
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-07-25 11:53:54 +0530

    Minor Changes

--- src/main/java/com/netsurfingzone/constant/ApplicationConstant.java
+++ src/main/java/com/netsurfingzone/constant/ApplicationConstant.java
@@ -12,7 +12,7 @@ public class ApplicationConstant {
 	public static final String KAFKA_LOCAL_SERVER_CONFIG = "localhost:9092";
 	public static final String TOPIC_NAME = "netsurfingzone-topic-1";
 	public static final String TOPIC_NAME_RF_TEMPLATE = "netsurfingzone-topic-2";
-	public static final String TOPIC_NAME_SUMMARY = "TOPIC_NAME_SUMMARY";
+	public static final String TOPIC_NAME_SUMMARY = "netsurfingzone-topic-1-summary";
 	public static final String TOPIC_NAME_SUMMARY_ATTACHMENTS = "netsurfingzone-topic-2-summary";
 	public static final String KAFKA_LISTENER_CONTAINER_FACTORY = "kafkaListenerContainerFactory";
 	public static final String KAFKA_LISTENER_CONTAINER_FACTORY_RF_V2 = "kafkaListenerContainerFactory1";
