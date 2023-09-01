cd277e989fbd29e9f4ab8d13583613189bf3c2bd
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-09-01 18:36:19 +0530

    Latest Code

--- src/main/java/com/netsurfingzone/config/SpringKafkaConfig.java
+++ src/main/java/com/netsurfingzone/config/SpringKafkaConfig.java
@@ -54,7 +54,7 @@ public class SpringKafkaConfig {
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "com.netsurfingzone.dto");
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "com.netsurfingzone.dto.Notify");
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-//		configMap.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,"700000");
+		configMap.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,"700000");
 //		configMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");
 //		configMap.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,"1000");
 //		configMap.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG,"3600000");
@@ -74,7 +74,7 @@ public class SpringKafkaConfig {
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "com.netsurfingzone.dto");
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "com.netsurfingzone.dto.SummaryPayload");
 		configMap.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-//		configMap.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,"700000");
+		configMap.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,"700000");
 //		configMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");
 //		configMap.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,"1000");
 //		configMap.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG,"3600000");
