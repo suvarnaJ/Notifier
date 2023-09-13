f7aa44eaa19b989b7f37976bbbba8962a3c2171a
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-09-13 15:48:10 +0530

    Extension_Work_Protected

--- src/main/java/com/netsurfingzone/main/SpringMain.java
+++ src/main/java/com/netsurfingzone/main/SpringMain.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.security.Security;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.netsurfingzone.*")
 public class SpringMain {
 	public static void main(String[] args) {
+		Security.setProperty("jdk.tls.disabledAlgorithms","SSLv3, RC4, DES, MD5withRSA, DH keySize < 1024,EC keySize < 224, 3DES_EDE_CBC, anon, NULL");
 		SpringApplication.run(SpringMain.class, args);
 	}
 }
